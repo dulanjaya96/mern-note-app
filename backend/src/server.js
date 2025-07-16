8638ddb0a1d83be59f3ad90c776d3c2e0c3aad9c
dulanjaya96 <nc.dulanjaya@gmail.com>
2025-07-16 13:59:44 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'; 
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
@@ -10,20 +11,36 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Get the current directory path
 
-connectDB();
+
 
 // Middleware
-app.use(cors({
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
     origin: 'http://localhost:5173', // Allow requests from the frontend    
-}));
+    })
+  );
+}
+
+
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../frontend/dist"))); // Serve static files from the frontend build directory
+
+    app.get('/{*any}', (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend","dist","index.html")); // Serve the index.html file for all other routes
+});
+}
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
     console.log('Server is running on port PORT:', PORT);
  });
-
+});
 
